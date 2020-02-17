ea915f34b68301bf44bc94daec2ba4c2bd93b063
George Nance <george.nance@blueworldinc.com>
2020-02-17 14:58:43 -0700

    service workers!

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,17 @@
 module.exports = {
+  pwa: {
+    name: "George Nance",
+    themeColor: "#d72323",
+    msTileColor: "#d72323",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // // configure the workbox plugin
+    // workboxPluginMode: "InjectManifest",
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: "dev/sw.js"
+    //   // ...other Workbox options...
+    // }
+  }
 };
